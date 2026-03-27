d6e9aea3e1f8619926104aefb338b9e1d4ffa795
Chhaya Pandey <np03cs5a240001@heraldcollege.edu.np>
2026-03-28 02:44:55 +0545

    Fixed the categoires.controller.ts file from errors

---
+++ app-backend/src/modules/categories/categories.controller.ts
@@ -0,0 +1,48 @@
+import type { Request, Response } from 'express';
+import { deleteCategory, getAllCategory, getCategory, postCategory, putCategory } from './categories.service.js';
+
+export const postCategoryController= async(req:Request, res:Response)=>{
+    try{
+        const response = await postCategory(req.body);
+        res.status(201).json(response);
+    }
+    catch(error){
+         res.status(400).json({ message: error});
+    }
+}
+
+export const getCategoryController= async(req:Request, res:Response)=>{
+    try{
+        const response= await getCategory({category_id: parseInt(req.params.id || "0")});
+        res.status(201).json(response);
+    }
+    catch(error){
+        res.status(400).json({message:error});
+    }
+};
+export const getAllCategoryController = async(res:Response)=>{
+    try{
+        const response= getAllCategory();
+        res.status(201).json(response);
+    }
+    catch(error){
+        res.status(400).json({message:error});
+    }
+} ;
+export const putCategoryController= async(req:Request, res: Response)=>{
+    try{
+        const response= await putCategory({...req.body, category_id: parseInt(req.params.id || "0")});
+        res.status(201).json(response);
+    }
+    catch(error){
+        res.status(400).json({message:error});
+    }
+};
+export const deleteCategoryController= async(req:Request, res:Response)=>{
+    try{
+        const response= await deleteCategory({ category_id: parseInt(req.params.id || "0") });
+        res.status(201).json(response)
+    }catch(error){
+        res.status(400).json({message:error});
+    }
+};
