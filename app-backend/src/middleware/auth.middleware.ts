1e985c421eea9a3d326ce1712866f3b3c0e01a56
Chhaya Pandey <np03cs5a240001@heraldcollege.edu.np>
2026-03-28 02:58:06 +0545

    fixed the token error shown in middleware/auth.middleware, error still not fixed, worked on categories.routes.ts

--- app-backend/src/middleware/auth.middleware.ts
+++ app-backend/src/middleware/auth.middleware.ts
@@ -1,12 +1,9 @@
 import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET;
-
-if (!JWT_SECRET) throw new Error("JWT_SECRET is not defined");
-
-
 export const protect = (req: Request, res: Response, next: NextFunction) => {
+  const JWT_SECRET = process.env.JWT_SECRET!;
+  
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
@@ -16,7 +13,7 @@ export const protect = (req: Request, res: Response, next: NextFunction) => {
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, JWT_SECRET as string);
+    const decoded = jwt.verify(token, JWT_SECRET);
     (req as any).user = decoded;
     next();
   } catch (error) {
---
+++ app-backend/src/modules/categories/categories.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { protect } from '../../middleware/auth.middleware.js';
+import { deleteCategoryController, getAllCategoryController, getCategoryController, postCategoryController, putCategoryController } from './categories.controller.js';
+
+
+const router = Router();
+
+router.post("/",protect, postCategoryController);
+router.get("/", protect, getAllCategoryController);
+router.get("/:id", protect, getCategoryController);
+router.put("/:id", protect, putCategoryController);
+router.delete(":/id", protect , deleteCategoryController);
+
+export default router;
