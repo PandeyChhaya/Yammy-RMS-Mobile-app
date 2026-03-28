f33b2f275c5d6a267699f60b200cac46a1101d4d
Chhaya Pandey <np03cs5a240001@heraldcollege.edu.np>
2026-03-28 17:34:03 +0545

    Fixed tables.types.ts after some extensive research

--- app-backend/src/modules/tables/tables.types.ts
+++ app-backend/src/modules/tables/tables.types.ts
@@ -1,20 +1,20 @@
 export interface PostTable{
-    table_number: number,
-    floor?: number,
+    table_number: string,
+    floor?: string,
     capacity?: number,
     table_status?: string,
-    order: string,
-    reservation: string,
 };
 export interface GetTable{
     table_id: number,
 };
-export interface GetAllTable{
-    table_number: number,
-};
 export interface PutTable{
     table_id: number,
+    floor?: string,
+    capacity:number,
+    table_status: string
+
 };
 export interface DeleteTable{
     table_id: number,
+
 };
