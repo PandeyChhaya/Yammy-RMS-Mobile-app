69daee55f385bdfab27af72fdeb805fb0549e339
Chhaya Pandey <np03cs5a240001@heraldcollege.edu.np>
2026-03-28 15:23:20 +0545

    Fixed menu-items-routes.ts after some extensive research

--- app-backend/src/modules/menu-items/menu-items.routes.ts
+++ app-backend/src/modules/menu-items/menu-items.routes.ts
@@ -1,14 +1,13 @@
 import { Router } from 'express';
 import { protect } from '../../middleware/auth.middleware.js';
-import { getCategoryController } from '../categories/categories.controller.js';
-import { deleteMenuItemsController, getAllMenuItemsController, postMenuItemsController, putMenuItemsController } from './menu-items.controller.js';
+import { deleteMenuItemsController, getAllMenuItemsController, getMenuItemsController, postMenuItemsController, putMenuItemsController } from './menu-items.controller.js';
 
 const router = Router();
 
 router.post('/',protect,postMenuItemsController);
-router.get('/.id',protect, getCategoryController);
+router.get('/:id',protect, getMenuItemsController);
 router.get('/', protect, getAllMenuItemsController);
-router.put('/.id',protect, putMenuItemsController);
-router.delete('/.id', protect, deleteMenuItemsController);
+router.put('/:id',protect, putMenuItemsController);
+router.delete('/:id', protect, deleteMenuItemsController);
 
 export default router;
