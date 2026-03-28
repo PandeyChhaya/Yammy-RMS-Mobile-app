2a352768d7f5f24039684673a9a9ede81af86e52
Chhaya Pandey <np03cs5a240001@heraldcollege.edu.np>
2026-03-28 23:36:04 +0545

    First attempt at tables.controller.ts

---
+++ app-backend/src/modules/tables/tables.controller.ts
@@ -0,0 +1,48 @@
+import type { Request, Response } from 'express';
+import { deleteTable, getAllTable, getTable, postTable, putTable } from './tables.service.js';
+
+export const postTableController= async(req:Request, res:Response)=>{
+    try{
+        const response = await postTable(req.body);
+        res.status(201).json(response);
+    }
+    catch(error){
+         res.status(400).json({ message: error});
+    }
+}
+
+export const getTableController= async(req:Request, res:Response)=>{
+    try{
+        const response= await getTable({table_id: parseInt(String(req.params.id))});
+        res.status(201).json(response);
+    }
+    catch(error){
+        res.status(400).json({message:error});
+    }
+};
+export const getAllTableController = async(res:Response)=>{
+    try{
+        const response= getAllTable();
+        res.status(201).json(response);
+    }
+    catch(error){
+        res.status(400).json({message:error});
+    }
+} ;
+export const putTableController= async(req:Request, res: Response)=>{
+    try{
+        const response= await putTable({...req.body, table_id: parseInt(String(req.params.id))});
+        res.status(201).json(response);
+    }
+    catch(error){
+        res.status(400).json({message:error});
+    }
+};
+export const deleteTableController= async(req:Request, res:Response)=>{
+    try{
+        const response= await deleteTable({ table_id: parseInt(String(req.params.id)) });
+        res.status(201).json(response)
+    }catch(error){
+        res.status(400).json({message:error});
+    }
+};
