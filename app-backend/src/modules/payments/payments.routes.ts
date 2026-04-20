82922c088742c2a7918e76c1775522288241093f
Chhaya Pandey <np03cs5a240001@heraldcollege.edu.np>
2026-04-20 14:26:55 +0545

    Minimal changes at payment.routes.ts

--- app-backend/src/modules/payments/payments.routes.ts
+++ app-backend/src/modules/payments/payments.routes.ts
@@ -1,13 +1,28 @@
 import { Router } from 'express';
 import { protect } from '../../middleware/auth.middleware.js';
-import { deletePaymentController, getAllPaymentsController, getPaymentController, postPaymentController, putPaymentController } from './payments.controller.js';
+import {
+    deletePaymentController,
+    esewaFailureCallback,
+    esewaSuccessCallback,
+    getAllPaymentsController,
+    getPaymentController,
+    initiateEsewaController,
+    postPaymentController,
+    putPaymentController,
+    verifyEsewaController,
+} from './payments.controller.js';
 
 const router = Router();
 
 router.post('/', protect, postPaymentController);
 router.get('/', protect, getAllPaymentsController);
 router.put('/:id', protect, putPaymentController);
-router.delete('/:id', protect,deletePaymentController);
+router.delete('/:id', protect, deletePaymentController);
 router.get('/:id', protect, getPaymentController);
+
+router.post('/esewa/initiate', protect, initiateEsewaController);
+router.post('/esewa/verify', protect, verifyEsewaController);
+router.get('/esewa/callback/success', esewaSuccessCallback);
+router.get('/esewa/callback/failure', esewaFailureCallback);
 
 export default router;
