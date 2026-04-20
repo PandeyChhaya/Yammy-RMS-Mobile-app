88b84026bd3ddb7cc08f6500be0c7b7d3ef6bafe
Chhaya Pandey <np03cs5a240001@heraldcollege.edu.np>
2026-04-20 14:15:17 +0545

    Remade Payment.types.ts

--- app-backend/src/modules/payments/payments.types.ts
+++ app-backend/src/modules/payments/payments.types.ts
@@ -1,21 +1,31 @@
-export interface PostPayment{
-    order_id: number,
-    payment_method: string,
-    amount_paid: number,
-    change_given: number,
-    transaction_ref: string,
-};
-export interface GetPayment{
-    payment_id: number,
-};
-export interface PutPayment{
-    payment_status: string,
-    transaction_ref: string,
-    payment_id: number,
+export interface PostPayment {
+    order_id: number;
+    payment_method: string;
+    amount_paid: number;
+    change_given: number;
+    transaction_ref: string;    
 }
 
-;
-export interface DeletePayment{
-    payment_id: number,
+export interface GetPayment {
+    payment_id: number;
+}
 
-};
+export interface PutPayment {
+    payment_status: string;
+    transaction_ref: string;
+    payment_id: number;
+}
+
+export interface DeletePayment {
+    payment_id: number;
+}
+
+export interface InitiateEsewa {
+    order_id: number;
+    amount: number;
+}
+
+export interface VerifyEsewa {
+    transaction_ref: string;
+    amount: number;
+}
