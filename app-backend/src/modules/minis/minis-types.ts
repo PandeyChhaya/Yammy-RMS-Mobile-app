5187b4d1e09bac4d96bf825e87be9afbe0bffdef
Chhaya Pandey <np03cs5a240001@heraldcollege.edu.np>
2026-04-23 15:52:43 +0545

    made minis.types.ts

---
+++ app-backend/src/modules/minis/minis-types.ts
@@ -0,0 +1,15 @@
+export interface CreateMini {
+  user_id: number;
+  title: string;
+  description?: string;
+}
+
+export interface UpdateMiniStatus {
+  mini_id: number;
+  status: 'approved' | 'rejected';
+  rejection_reason?: string;
+}
+
+export interface GetMini {
+  mini_id: number;
+}
